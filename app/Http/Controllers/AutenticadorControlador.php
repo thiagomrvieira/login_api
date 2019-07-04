3205b0fa49047e349ad41c7cd617262f1de0dca4
Thiago Maciel <thiagomrvieira@gmail.com>
2019-07-04 11:16:28 -0300

    Criando o controlador para autenticação

---
+++ app/Http/Controllers/AutenticadorControlador.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class AutenticadorControlador extends Controller
+{
+    public function registro(Request $request){
+        
+        //Valida os dados que serão recebidos: nome, email e senha
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        //Se ele passar pela validação, cria o usuário:
+        $user =  new User([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> bcrypt($request->password)
+        ]); 
+        
+        $user->save();
+
+        return response()->json([
+            'res'=>'Usuário criado com sucesso!'
+        ], 201); // Código de Status de objeto criado
+
+    }
+
+    public function login(Request $request){
+        
+        //Valida os dados que serão recebidos: email e senha
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        $credenciais = [
+            'email' => $request->email, 
+            'password' => $request->password
+        ];
+
+        //retorna true ou false
+        //Auth::attempt($credenciais);
+
+        //Se não conseguir autenticar:
+        if (!Auth::attempt($credenciais)) {
+            return reponse()->json([
+                'res' => 'Acesso negado!'
+            ], 401);
+        } 
+
+        //Se autenticar, cria-se um Token:
+        
+        //Acessa o usuário logado
+        $user = $request->user();
+        //armazena o token na variável
+        $token = $user->createToken('Token de acesso')->accessToken();
+        //Retorna o token como response
+        return response()->json([
+            'token' => $token
+        ], 200);
+
+
+    }
+
+    public function logout(Request $request){
+        //Acessa o usuário logado e revoga o token
+        $request->user()->token()->revoke();
+        return response()->json([
+            'res' => 'Deslogado com sucesso!'
+        ]);
+    }
+}
