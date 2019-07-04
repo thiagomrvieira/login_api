be0460e05544b05985ed41e6db35d8d69f35df79
Thiago Maciel <thiagomrvieira@gmail.com>
2019-07-04 11:43:28 -0300

    Configurando rotas

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function(){
+    Route::post('registro', 'AutenticadorControlador@registro');
+    Route::post('login', 'AutenticadorControlador@login');
+    
+    Route::middleware('auth:api')->group(function(){
+        Route::post('logout', 'AutenticadorControlador@logout');
+    });
+});
+
+    Route::get('produtos', 'ProdutosControlador@index')->middleware('auth:api');
